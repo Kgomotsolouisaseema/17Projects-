d9c16f95dd9e923014f7b172d504e346b2f48c07
kgomotso <kgomotsolouisaseema@gmail.com>
2024-06-04 12:41:58 +0200

    lorem

--- 15-lorem/script.js
+++ 15-lorem/script.js
@@ -1,15 +1,24 @@
+ //The getLoremIpsum function takes the number of paragraphs as a parameter.
 const getLoremIpsum = (numberOfParagraphs) => {
+  //It uses the fetch function to make a GET request to the Bacon Ipsum API, specifying the type of text and the number of paragraphs.
+
   fetch(
     `https://baconipsum.com/api/?type=meat-and-filler&paras=${numberOfParagraphs}`)
+    //It then processes the response as JSON and passes the resulting array of lorem ipsum paragraphs to the updateResult function.
+
     .then((response) => response.json())
     .then((loremIpTextArray) => {
       updateResult(loremIpTextArray);
     });
 };
 
+//The updateResult function takes an array of text paragraphs as input.
+
 const updateResult = textArray => {
+  //It gets the HTML element with the id "result" and adds the "show" class to it.
   const resultElement = document.getElementById("result");
   resultElement.classList.add('show');
+  //It then clears the inner HTML of the result element and populates it with the paragraphs of lorem ipsum text wrapped in <p> tags.
   resultElement.innerHTML = "";
   resultElement.innerHTML = textArray
     .map((paragraph) => `<p>${paragraph}</p>`)
@@ -18,10 +27,16 @@ const updateResult = textArray => {
  
 };
 
+
+//These lines retrieve the button and input elements from the HTML document.
 const getloremIpsumBtnElement = document.getElementById("getLoremIp");
 const paragraphsCountElement = document.getElementById("paragraphsCount");
 
+//They add an event listener to the button so that when it's clicked, 
+//the getLoremIpsum function is called with the number of paragraphs specified in the input element, and then the input element is cleared.
 getloremIpsumBtnElement.addEventListener("click", () => {
   getLoremIpsum(parseInt(paragraphsCountElement.value));
   paragraphsCountElement.value = "";
 });
+ 
+
