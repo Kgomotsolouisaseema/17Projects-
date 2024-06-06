5242748be17b0168656797c0eecd4d87c7873bdd
kgomotso <kgomotsolouisaseema@gmail.com>
2024-06-06 13:35:33 +0200

    hamburger doesnt open at breakpoint

--- 18-responsiveNav/script.js
+++ 18-responsiveNav/script.js
@@ -1,11 +1,26 @@
-const responsiveMenuBtn = document.getElementById('responsiveMenuBtn')
+// const responsiveMenuBtn = document.getElementById('responsiveMenuBtn')
 
-const navBarlinks = document.querySelector('.navbar-links')
+// const navBarlinks = document.querySelector('.navbar-links')
 
 
-responsiveMenuBtn.addEventListener('click' , () =>{
-    navBarlinks.classList.toggle('open');
-    responsiveMenuBtn.classList.toggle('open');
+// responsiveMenuBtn.addEventListener('click' , () =>{
+//     navBarlinks.classList.toggle('open');
+//     responsiveMenuBtn.classList.toggle('open');
 
+// });
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('responsiveMenuBtn');
+    const navbar = document.querySelector('.navbar');
+    const navbarLinks = document.querySelector('.navbar-links');
+
+    menuButton.addEventListener('click', function() {
+        navbar.classList.toggle('open');
+        navbarLinks.classList.toggle('open');
+        menuButton.classList.toggle('open');
+    });
 });
 
